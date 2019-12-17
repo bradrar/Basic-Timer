0ad06dd1b8d72a530ec30704e304108ba36f7388
bradrar <bradbaguyo@gmail.com>
2019-12-18 01:17:52 +0800

    fix ui

--- src/components/Timer component/Timer.js
+++ src/components/Timer component/Timer.js
@@ -6,6 +6,7 @@ import "../styles/Timer.css";
 const Timer = () => {
   const [seconds, setSeconds] = useState(300);
   const [status, setStatus] = useState(false);
+  const [timerHasFinished, setTimerHasFinished] = useState(false);
 
   const handleChange = props => {
     setSeconds(props);
@@ -15,9 +16,9 @@ const Timer = () => {
     if (seconds !== "0") {
       setStatus(true);
       document.getElementById("sound").pause();
-      document.getElementById("sound").currentTime = 0; //add this to reset audio  
+      document.getElementById("sound").currentTime = 0; //add this to reset audio
     }
-   };
+  };
 
   const pauseTimer = () => {
     setStatus(false);
@@ -25,18 +26,26 @@ const Timer = () => {
 
   const stopTimer = () => {
     setStatus(false);
+    setTimerHasFinished(false);
     setSeconds(0);
     document.getElementById("sound").pause();
     document.getElementById("sound").currentTime = 0; //add this to reset audio
   };
 
   useEffect(() => {
-    if (seconds === 0) {
-      setStatus(false)
+    if (timerHasFinished) {
       document.getElementById("sound").play();
-      return
     }
-    
+  }, [timerHasFinished]);
+
+  useEffect(() => {
+    if (seconds === 0) {
+      setStatus(false);
+      setTimerHasFinished(true);
+
+      return;
+    }
+
     const intervalID = setInterval(() => {
       if (status) {
         setSeconds(countdown => countdown - 1);
@@ -49,9 +58,7 @@ const Timer = () => {
 
   return (
     <div className="timer">
-      <Display seconds={seconds}
-      status={status}
-      handleChange={handleChange} />
+      <Display seconds={seconds} status={status} handleChange={handleChange} />
       <Controls
         startTimer={startTimer}
         stopTimer={stopTimer}
@@ -59,8 +66,8 @@ const Timer = () => {
         status={status}
         seconds={seconds}
       />
-       {/* this is the sound, it is hidden */}
-       <div>
+      {/* this is the sound, it is hidden */}
+      <div>
         <audio id="sound" type="audio/mpeg">
           <source src="http://soundbible.com/mp3/Metal%20Gong-SoundBible.com-1270479122.mp3" />
         </audio>
